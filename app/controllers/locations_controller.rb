a3e8925368d5eb071e26a0f375a81630805312a4
Frank DelPidio <frankdaedalus@gmail.com>
2015-05-28 17:17:19 -0400

    Controllers: create create action for locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,5 +5,16 @@ class LocationsController < ApplicationController
   end
 
   def create
+    @location = Location.new(location_params)
+    if @location.save
+      flash[:success] = "Location Created!"
+    end
+      redirect_to :new_game
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:address, :city, :zip_code)
   end
 end
