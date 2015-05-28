56dfa2b47179d74f880cbf58b420425e70aff391
Frank DelPidio <frankdaedalus@gmail.com>
2015-05-28 18:57:00 -0400

    Controllers: update games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,15 +3,37 @@ class GamesController < ApplicationController
   def nextgame
     @game = Game.last
     if @game.nil?
-      flash[:info] = "There are currently no scheduled games. Set one up!"
+      flash[:alert] = "There are currently no scheduled games. Set one up!"
       redirect_to :new_game
     else
       render :nextgame
     end
   end
 
+  def index
+    @games = Game.all
+  end
+
   def new
-    @game = Game.new
+    @game = Game.new()
+    @locations = Location.all
+    @game_types = GameType.all
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      flash[:success] = "Game created!"
+      redirect_to :games
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :game_type_id, :location_id, :date, :description)
   end
 
 end
