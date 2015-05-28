a2a28f3881ec262548dd1ae53226fafc24993074
Frank DelPidio <frankdaedalus@gmail.com>
2015-05-28 15:02:19 -0400

    create new game form

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,13 +4,14 @@ class GamesController < ApplicationController
     @game = Game.last
     if @game.nil?
       flash[:info] = "There are currently no scheduled games. Set one up!"
-      render :new
+      redirect_to :new_game
     else
       render :nextgame
     end
   end
 
   def new
+    @game = Game.new
   end
 
 end
