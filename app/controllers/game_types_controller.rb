ab1f94ce2d5cda8e0ba1ee8e0299d2071c02f6ed
Frank DelPidio <frankdaedalus@gmail.com>
2015-05-28 17:37:30 -0400

    Controllers: create create action for game_types

--- app/controllers/game_types_controller.rb
+++ app/controllers/game_types_controller.rb
@@ -5,5 +5,17 @@ class GameTypesController < ApplicationController
   end
 
   def create
+    @game_type = GameType.new(game_type_params)
+    if @game_type.save
+      flash[:success] = "Game type created!"
+    end
+    debugger
+    redirect_to :new_game
+  end
+
+  private
+
+  def game_type_params
+    params.require(:game_type).permit(:name, :min_num_players, :how_to_play_link)
   end
 end
