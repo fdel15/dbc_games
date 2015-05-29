d9501ffb1318b9a57cb4510ad66539d90629e78f
Frank DelPidio <frankdaedalus@gmail.com>
2015-05-29 13:11:26 -0400

    Models: add validations to GameType

--- app/models/game_type.rb
+++ app/models/game_type.rb
@@ -1,3 +1,7 @@
 class GameType < ActiveRecord::Base
   has_many :games
+
+  validates_presence_of :name, :min_num_players, :how_to_play_link
+  validates :name, uniqueness: { case_sensitive: false }
+
 end
