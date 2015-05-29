ed3cd44617d785de14467527dead5de6366f3e6a
Frank DelPidio <frankdaedalus@gmail.com>
2015-05-29 13:50:53 -0400

    Models: get geocoordinates on creation of location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,15 @@
 class Location < ActiveRecord::Base
   has_many :games
+  geocoded_by :address_and_zip_code
+  after_validation :geocode
 
   validates_presence_of :address, :city, :zip_code
 
   def address_and_city
     "#{address}, #{city}"
+  end
+
+  def address_and_zip_code
+    "#{address}, #{zip_code}"
   end
 end
