14308a4422a9bd6ffb3874b677e2c64661118732
Frank DelPidio <frankdaedalus@gmail.com>
2015-05-31 19:16:34 -0400

    Models: set up associations between games and users

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,8 @@
 class Game < ActiveRecord::Base
   belongs_to :game_type
   belongs_to :location
+  has_many :games_users
+  has_many :users through: :games_users
 
   validates_presence_of :name, :game_type, :location, :date
 end
