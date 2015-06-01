ba96c5bb60541cb6690dffc54d9563bbfaa8b422
Frank DelPidio <frankdaedalus@gmail.com>
2015-06-01 14:01:46 -0400

    Model: add format time and format date methods to game

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,4 +5,12 @@ class Game < ActiveRecord::Base
   has_many :users, through: :games_users
 
   validates_presence_of :name, :game_type, :location, :date
+
+  def formatted_date
+    date.strftime('%a %b %d %Y')
+  end
+
+  def formatted_time
+    date.strftime("%I:%M %p")
+  end
 end
