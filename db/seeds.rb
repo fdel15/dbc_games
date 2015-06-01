276bc0942b8a1b38e079fd6cc721fa2daa1dfd8e
Frank DelPidio <frankdaedalus@gmail.com>
2015-06-01 00:25:24 -0400

    create a new game in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,5 @@
 location = Location.create(address: '63 Wall St.', city: 'New York', zipcode: '10005' )
 
 game_type = GameType.create(name: 'Poker', min_num_players: 2, how_to_play_link: 'http://www.pokernews.com/poker-rules/texas-holdem.htm')
+
+Game.create(name: "test_game", game_type_id: game_type.id, location_id: location.id, description: "TEST TEST TEST", date: Time.now)
