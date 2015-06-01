93b8fc792f14a5a76b1b3eda8d02002adbb87051
Frank DelPidio <frankdaedalus@gmail.com>
2015-05-31 20:17:37 -0400

    fix typo

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ActiveRecord::Base
   belongs_to :game_type
   belongs_to :location
   has_many :games_users
-  has_many :users through: :games_users
+  has_many :users, through: :games_users
 
   validates_presence_of :name, :game_type, :location, :date
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :games_users
-  has_many :games through: :games_users
+  has_many :games, through: :games_users
 end
