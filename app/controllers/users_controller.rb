89d8a97fa2fa1701ef60cec5e5351a0a1f7dae6f
Frank DelPidio <frankdaedalus@gmail.com>
2015-06-02 16:49:32 -0400

    Controllers: create update action for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      flash[:success] = "Profile Updated!"
+      render :show
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :cohort, :employer, :job_title, :quirk, :tweet_advice)
+  end
+
 end
